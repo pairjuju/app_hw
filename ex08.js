2da5263d587082274ced315a64b709cac5a43877
pairjuju <mizumizukooo@gmail.com>
2017-03-12 16:15:40 +0800

    Add files via upload

--- ex08.js
+++ ex08.js
@@ -1,10 +1,21 @@
-var addclosure=(()=>{
- var counter =0;
- return ()=>{
-   counter+=1;
-   console.log(counter);
- }
-})();
-addclosure();
-addclosure();
-addclosure();
+var personobj = {
+	firstname: "Simon",
+	fullname: "",
+	age: 37,
+	married: true,
+	hasOwnHair: null,
+	children: [{
+		firstname: "Erica"
+	}, {
+		firstname: "Isobel"
+	}]
+};
+
+var personjson = JSON.stringify(personobj);
+console.log(personobj);
+console.log(personjson);
+
+var personjs=JSON.parse(personjson);
+console.log(personobj);
+console.log(personjson);
+console.log(persojs);
